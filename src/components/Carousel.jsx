bd2a4a8f200f866ed822d8f83d770fba0d9f57da
hasibur14 <hasib141415@gmail.com>
2024-05-04 16:39:40 +0600

    added banner section

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,57 @@
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import bannerImg1 from '../assets/images/carousel1.jpg';
+import bannerImg2 from '../assets/images/carousel2.jpg';
+import bannerImg3 from '../assets/images/carousel3.jpg';
+import Slide from './Slider';
+
+
+const Carousel = () => {
+    return (
+        <>
+            <div className='mx-6 my-10 rounded-lg'>
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    loop={true}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <Slide
+                         image={bannerImg1}
+                         text='Get Your Web Development Projects Done in Minute '
+                         ></Slide>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Slide 
+                        image={bannerImg2}
+                        text='Get Your Graphics Design Projects Done in Minute '
+                        ></Slide>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Slide 
+                        image={bannerImg3}
+                        text='Get Your Digital Marketing  Projects Done in Minute '
+                        ></Slide>
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+        </>
+    );
+};
+
+export default Carousel;
